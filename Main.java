fef28969d2a294a002ba0b29cf4a1c02dcff90af
Guilherme Montinho Langa <133558093+p15t0@users.noreply.github.com>
2025-02-25 10:53:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,37 +9,48 @@ Code, Compile, Run and Debug online from anywhere in world.
 import java.util.Scanner;
 
 class Conta {
-    int num;
-    String nome;
-    double limite;
-    double saldo;
-    
-    void Sacar(double v) {
-        saldo -= v;
-    }
-    
-    void Depositar(double v) {
-        saldo += v;
-    }
+	int num;
+	String nome;
+	double limite;
+	double saldo;
+
+	boolean Sacar(double v) {
+		if (v > saldo) {
+			System.out.println("Saldo insuficiente! VocC* tem R$"+saldo);
+			return true;
+		}
+		else if (v < 0) {
+			System.out.println("Digite um valor vC!lido");
+			return true;
+		}
+		else {
+			saldo -= v;
+			return false;
+		}
+	}
+
+	void Depositar(double v) {
+		saldo += v;
+	}
 }
 
 
 
 public class Main
 {
-    public static void Menu() {
-	System.out.println("\nMenu:");
-	System.out.println("\n[1] Ver conta");
-	System.out.println("[2] Sacar");
-	System.out.println("[3] Depositar");
-	System.out.println("[0] Sair\n");
-    }
-    
+	public static void Menu() {
+		System.out.println("\nMenu:");
+		System.out.println("\n[1] Ver conta");
+		System.out.println("[2] Sacar");
+		System.out.println("[3] Depositar");
+		System.out.println("[0] Sair\n");
+	}
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-	    Conta conta = new Conta();
-		
-		System.out.println("Digite o número da conta:");
+		Conta conta = new Conta();
+
+		System.out.println("Digite o nC:mero da conta:");
 		conta.num = scan.nextInt();
 
 		System.out.println("Digite o limite da conta:");
@@ -49,28 +60,32 @@ public class Main
 		double saque;
 		double deposito;
 		conta.saldo = 0;
-        do {
-            Menu();
+		boolean restart;
+		do {
+			Menu();
 			escolha = scan.nextInt();
 			if (escolha == 1) {
-			    System.out.println("\nSaldo:"+conta.saldo);
+				System.out.println("\nSaldo: "+conta.saldo);
 			}
 			else if(escolha == 2) {
-				System.out.println("\nEscreva o quanto deseja sacar:");
-				saque = scan.nextDouble();
-				conta.Sacar(saque);
+				do {
+					System.out.println("\nEscreva o quanto deseja sacar:");
+					saque = scan.nextDouble();
+					restart = conta.Sacar(saque);
+				} while (loop == true);
+				loop = true;
+				System.out.println("\nSaldo atual: "+conta.saldo);
 			}
 			else if(escolha == 3) {
 				System.out.println("\nEscreva o quanto deseja depositar:");
 				deposito = scan.nextDouble();
-				conta.Depositar(deposito);    
+				conta.Depositar(deposito);
 			}
 			else if(escolha != 0) {
-			    System.out.println("\nEscolha inválida! Digite outro número!");
+				System.out.println("\nEscolha invC!lida! Digite outro nC:mero!");
 			}
 		} while(escolha != 0);
 
-		
+
 	}
 }
-
